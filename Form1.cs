c9d4738f32c1add5fd4809392c81d9dc11b7d334
khiemld <khiemit240@gmail.com>
2023-10-06 05:27:40 +0700

    fix: tc_2,7, 8, 22, 28. 29, 12, 13

--- Form1.cs
+++ Form1.cs
@@ -36,10 +36,10 @@ namespace Buoi07_TinhToan3
 
         private void btnTinh_Click(object sender, EventArgs e)
         {
+            string so1 = txtSo1.Text;
+            string so2 = txtSo2.Text;
             try
             {
-                string so1 = txtSo1.Text;
-                string so2 = txtSo2.Text;
                 if( so1.Contains(",") || so1.Contains(".") )
                 {
                     decimal so1Dec = Convert.ToDecimal(so1);
@@ -47,18 +47,58 @@ namespace Buoi07_TinhToan3
                     {
                         
                         BigInteger so2Int = BigInteger.Parse(so2);
+                        
+                        
                         if (radCong.Checked) txtKq.Text = (so1Dec + Convert.ToDecimal(so2Int)).ToString();
                         else if (radTru.Checked) txtKq.Text = (so1Dec - Convert.ToDecimal(so2Int)).ToString();
                         else if (radNhan.Checked) txtKq.Text = (so1Dec * Convert.ToDecimal(so2Int)).ToString();
-                        else if (radChia.Checked && so2Int != 0) txtKq.Text = (so1Dec / Convert.ToDecimal(so2Int)).ToString();
+                        else if (radChia.Checked)
+                        {
+                            if (so2Int == 0)
+                            {
+                                DialogResult dr;
+                                dr = MessageBox.Show("Số chia phải khác 0", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                                if (dr == DialogResult.OK)
+                                    txtSo2.Focus();
+                            }
+                            else
+                            {
+                                txtKq.Text = (so1Dec / Convert.ToDecimal(so2Int)).ToString();
+                            }
+                        }
+                           
                     }
                     else if (so2.Contains(",") || so2.Contains(".")) 
                     {
                         decimal so2Dec = decimal.Parse(so2);
+                        if(so2Dec == 0)
+                        {
+                            DialogResult dr;
+                            dr = MessageBox.Show("Số chia phải khác 0", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                            if (dr == DialogResult.OK)
+                                txtSo2.Focus();
+                        }
+                        
                         if (radCong.Checked) txtKq.Text = (so1Dec + so2Dec).ToString();
                         else if (radTru.Checked) txtKq.Text = (so1Dec - so2Dec).ToString();
                         else if (radNhan.Checked) txtKq.Text = (so1Dec * so2Dec).ToString();
-                        else if (radChia.Checked && so2Dec != 0) txtKq.Text = (so1Dec / so2Dec).ToString();
+                        else if (radChia.Checked)
+                        {
+                            if (so2Dec == 0)
+                            {
+                                DialogResult dr;
+                                dr = MessageBox.Show("Số chia phải khác 0", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                                if (dr == DialogResult.OK)
+                                    txtSo2.Focus();
+                            }
+                            else
+                            {
+                                txtKq.Text = (so1Dec / so2Dec).ToString();
+                            }
+                        }
 
                     }
                 }
@@ -67,28 +107,74 @@ namespace Buoi07_TinhToan3
                     BigInteger so1Int = BigInteger.Parse(so1);
                     if (so2.Contains(",") == false && so2.Contains(".") == false)
                     {
-
+                       
                         BigInteger so2Int = BigInteger.Parse(so2);
                         if (radCong.Checked) txtKq.Text = (so1Int + so2Int).ToString();
                         else if (radTru.Checked) txtKq.Text = (so1Int - so2Int).ToString();
                         else if (radNhan.Checked) txtKq.Text = (so1Int * so2Int).ToString();
-                        else if (radChia.Checked && so2Int != 0) txtKq.Text = (so1Int / so2Int).ToString();
+                        else if (radChia.Checked)
+                        {
+                            if (so2Int == 0)
+                            {
+                                DialogResult dr;
+                                dr = MessageBox.Show("Số chia phải khác 0", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                                if (dr == DialogResult.OK)
+                                    txtSo2.Focus();
+                            }
+                            else
+                            {
+                                txtKq.Text = (so1Int / so2Int).ToString();
+                            }
+                        }
 
                     }
                     else if (so2.Contains(",") || so2.Contains("."))
                     {
+                        
                         decimal so2Dec = decimal.Parse(so2);
                         if (radCong.Checked) txtKq.Text = (decimal.Parse(so1) + so2Dec).ToString();
                         else if (radTru.Checked) txtKq.Text = (decimal.Parse(so1) - so2Dec).ToString();
                         else if (radNhan.Checked) txtKq.Text = (decimal.Parse(so1) * so2Dec).ToString();
-                        else if (radChia.Checked && so2Dec != 0) txtKq.Text = (Convert.ToDecimal(so1Int) / so2Dec).ToString();
+                        else if (radChia.Checked)
+                        {
+                            if (so2Dec == 0)
+                            {
+                                DialogResult dr;
+                                dr = MessageBox.Show("Số chia phải khác 0", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                                if (dr == DialogResult.OK)
+                                    txtSo2.Focus();
+                            }
+                            else
+                            {
+                                txtKq.Text = (Convert.ToDecimal(so1Int) / so2Dec).ToString();
+                            }
+                        }
                     }
                 }
                 
             }
             catch(Exception ex) 
             {
-                MessageBox.Show(ex.ToString());
+                if (so1 == "" || so2 == "")
+                {
+                    MessageBox.Show("Dữ liệu nhập vào không chứa ký tự và không để trống", "Thông báo", MessageBoxButtons.OK);
+                    if(so1 == "")
+                    {
+                        txtSo1.Focus();
+                    }
+                    else if(so2 == "")
+                    {
+                        txtSo2.Focus();
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+               
             }
             //lấy giá trị của 2 ô số
            /* decimal so1, so2, kq = 0;
